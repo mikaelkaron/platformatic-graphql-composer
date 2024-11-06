7ec7f1ac577cc36580d72a83cf233db2cba08123
Mikael Karon <mikael@karon.se>
2024-11-06 23:04:06 +0000

    fix: correct data

--- web/artists/seed.ts
+++ web/artists/seed.ts
@@ -3,7 +3,7 @@ import { Entities } from '@platformatic/sql-mapper'
 
 const artists: object[] = [
   { id: '101', first_name: 'Christopher', last_name: 'Nolan', profession: 'Director' },
-  { id: '201', first_name: 'Luciano', last_name: 'Roberto', profession: 'Singer' },
+  { id: '201', first_name: 'Luciano', last_name: 'Pavarotti', profession: 'Singer' },
   { id: '301', first_name: 'Brian', last_name: 'Molko', profession: 'Singer' },
   { id: '401', first_name: 'Bruce', last_name: 'Dickinson', profession: 'Singer' },
   { id: '102', first_name: 'Roberto', last_name: 'Benigni', profession: 'Director' }
